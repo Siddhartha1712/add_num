5857b19facf1ed79384abbcb3c789992c5027455
Siddhartha1712 <its.trashi.17@gmail.com>
2025-06-19 22:25:41 +0530

    subtraction

--- add.py
+++ add.py
@@ -3,5 +3,9 @@ def add_two(a,b):
 
 a = input("Enter the value of a:")
 b = input("Enter the value of b:")
+
+def sub_two(a,b):
+    return a-b
  
 print("Result of a+b :",add_two(a,b))
+print("Result of a+b :",sub_two(a,b))
